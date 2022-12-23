cca3352b635cf973cc4ff55211f1ffe22deef183
Bryan M Joseph <bryanmatthewjoseph@gmail.com>
2022-12-22 17:59:22 -0700

    Added subscribe to zone specific topic on init -- added servo movement on message received -- added ability to run TMP117 with PCA9685

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
-
+#include <ESP32Servo.h>
+// Include Wire Library for I2C
+#include <Wire.h>
+// Include Adafruit PCA9685 Servo Library
+#include <Adafruit_PWMServoDriver.h>
 #include <Arduino.h>
 #include <WiFi.h>
 extern "C" {
@@ -6,17 +10,14 @@ extern "C" {
   #include "freertos/timers.h"
 }
 #include <AsyncMqttClient.h>
-#include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
-#include <SparkFun_TMP117.h> // Used to send and recieve specific information from our sensor
+#include <SparkFun_TMP117.h> // Used to send and recieve specific information from TMP117 sensor
 
+/**Define the Wi-Fi and MQTT BLOCK**/
 // Enter WiFi SSID and password
 #define WIFI_SSID "RT-AC1200_48_2G"
 #define WIFI_PASSWORD "RoxSun10122021"
-// char ssid[] = "RT-AC1200_48_2G";             // your network SSID (name)
-// char pass[] = "RoxSun10122021";    // your network password (use for WPA, or use as key for WEP)
-// int keyIndex = 0;                      // your network key Index number (needed only for WEP)
 
 // Raspberry Pi Mosquitto (MQTT) Broker
 #define MQTT_HOST IPAddress(192, 168, 50, 249)
@@ -25,9 +26,7 @@ extern "C" {
 // Temperature MQTT Topics
 #define MQTT_PUB_FTEMP "esp/sparkfuntmp117/fahrenheittemperature"
 #define MQTT_PUB_CTEMP "esp/sparkfuntmp117/celsiustemperature"
-
-// The default address of the device is 0x48 = (GND)
-TMP117 sensor; // Initalize sensor using I2C
+#define MQTT_SUB_ZONE1_COMMAND "esp/zone/1/command"
 
 // Variables to hold sensor readings
 float temperatureF;
@@ -40,6 +39,29 @@ TimerHandle_t wifiReconnectTimer;
 unsigned long previousMillis = 0;   // Stores last time temperature was published
 const long interval = 10000;        // Interval at which to publish sensor readings
 
+// The default address of the device is 0x48 = (GND)
+TMP117 sensor; // Initalize sensor using I2C
+
+/**Define the PCA9685 and SERVO BLOCK**/
+// Creat object to represent PCA9685 at default I2C address 0x40
+Adafruit_PWMServoDriver pca9685 = Adafruit_PWMServoDriver(0x40);
+
+// Depending on your servo make, the pulse width min and max may vary, you 
+// want these to be as small/large as possible without hitting the hard stop
+// for max range. You'll have to tweak them as necessary to match servos
+#define SERVOMIN  90 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  500 // This is the 'maximum' pulse length count (out of 4096)
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+
+// Define servo motor connections (expand as required)
+#define SER0  0   //Servo Motor 0 on connector 0
+#define SER1  8  //Servo Motor 1 on connector 8
+
+// Variables for Servo Motor positions (expand as required)
+int pwm0;
+int pwm1;
+
+/**INIT METHODS BLOCK**/
 // get sensor readings here
 void getSparkfunTMP117Readings() {
   // Tell TMP117 to begin measurements
@@ -51,13 +73,120 @@ void getSparkfunTMP117Readings() {
 }
 
 void connectToWifi() {
-  Serial.println("Connecting to Wi-Fi...");
+  Serial.println("Connecting to strongest Wi-Fi network...");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 
 void connectToMqtt() {
   Serial.println("Connecting to MQTT Broker...");
   mqttClient.connect();
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void executeServoOpenCommand() {
+  // Move Motor 1 from 90 to 0 degrees -- Motor 1 is currently holding the vent open
+  for (int posDegrees = 90; posDegrees >= 0; posDegrees--) {
+ 
+    // Determine PWM pulse width
+    pwm1 = map(posDegrees, 0, 180, SERVOMIN, SERVOMAX);
+    // Write to PCA9685
+    pca9685.setPWM(SER1, 0, pwm1);
+    // Print to serial monitor
+    // Serial.print("Motor 1 = ");
+    // Serial.println(posDegrees);
+    // delay(30);
+  }
+
+  // Move Motor 0 from 0 to 90 degrees -- Motor 0 will close vent after Motor 1 moves out of the way
+  for (int posDegrees = 0; posDegrees <= 90; posDegrees++) {
+ 
+    // Determine PWM pulse width
+    pwm0 = map(posDegrees, 0, 180, SERVOMIN, SERVOMAX);
+    // Write to PCA9685
+    pca9685.setPWM(SER0, 0, pwm0);
+    // Print to serial monitor
+    // Serial.print("Motor 0 = ");
+    // Serial.println(posDegrees);
+    // delay(30);
+  }
+}
+
+void executeServoCloseCommand() {
+  // Move Motor 0 from 90 to 0 degrees
+  for (int posDegrees = 90; posDegrees >= 0; posDegrees--) {
+ 
+    // Determine PWM pulse width
+    pwm0 = map(posDegrees, 0, 180, SERVOMIN, SERVOMAX);
+    // Write to PCA9685
+    pca9685.setPWM(SER0, 0, pwm0);
+    // Print to serial monitor
+    // Serial.print("Motor 0 = ");
+    // Serial.println(posDegrees);
+    // delay(30);
+  }
+ 
+  // Move Motor 1 from 0 to 90 degrees
+  for (int posDegrees = 0; posDegrees <= 90; posDegrees++) {
+ 
+    // Determine PWM pulse width
+    pwm1 = map(posDegrees, 0, 180, SERVOMIN, SERVOMAX);
+    // Write to PCA9685
+    pca9685.setPWM(SER1, 0, pwm1);
+    // Print to serial monitor
+    // Serial.print("Motor 1 = ");
+    // Serial.println(posDegrees);
+    // delay(30);
+  }
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.println("Message received from subscribed topic");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+}
+
+void mqttMessageCallback(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String recMessage;
+  
+  for (int i = 0; i < len; i++) {
+    Serial.print(payload[i]);
+    recMessage += payload[i];
+  }
+  Serial.println();
+
+  // A message received on the topic esp/zone/1/command, check if the message is either "on" or "off" 
+  // Causes servo movement according to the message
+  Serial.print("Changing output to ");
+  if (recMessage == "open") {
+    Serial.println("open");
+    executeServoOpenCommand();
+  }
+  else if (recMessage == "close") {
+    Serial.println("close");
+    executeServoCloseCommand();
+  }
 }
 
 // this switch is going to call a secondary action if a WiFi event occurs
@@ -82,6 +211,11 @@ void onMqttConnect(bool sessionPresent) {
   Serial.println("Connected to MQTT Broker.");
   Serial.print("Session present: ");
   Serial.println(sessionPresent);
+
+  // in the MQTT connect call the subscribe as well
+  uint16_t packetIdSub = mqttClient.subscribe(MQTT_SUB_ZONE1_COMMAND, 2);
+  Serial.print("Subscribing at QoS 2, packetId: ");
+  Serial.println(packetIdSub);
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
@@ -96,15 +230,24 @@ void onMqttPublish(uint16_t packetId) {
   Serial.print("  packetId: ");
   Serial.println(packetId);
 }
+/**END METHODS BLOCK**/
 
 void setup() {
   //Initialize serial and wait for port to open:
-  Wire.begin();
+  Wire1.begin();
+  // Serial monitor setup
   Serial.begin(115200);
-  Wire.setClock(400000);   // Set clock speed to be the fastest for better communication (fast mode)
+  // Initialize PCA9685
+  pca9685.begin();
+  // Set PWM Frequency to 50Hz
+  pca9685.setPWMFreq(SERVO_FREQ);
+
+  Wire1.setClock(400000);   // Set clock speed to be the fastest for better communication (fast mode)
   Serial.println();
 
-  Serial.println("TMP117: Basic Readings");
+  // Print to monitor
+  Serial.println("TMP117: Basic Readings with PCA9685 Servo Test");
+  
   if (sensor.begin() == true) // Function to check if the sensor will correctly self-identify with the proper Device ID/Address
   {
     Serial.println("Begin");
@@ -124,8 +267,9 @@ void setup() {
 
   mqttClient.onConnect(onMqttConnect);
   mqttClient.onDisconnect(onMqttDisconnect);
-  //mqttClient.onSubscribe(onMqttSubscribe);
-  //mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onSubscribe(onMqttSubscribe);
+  // mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onMessage(mqttMessageCallback);
   mqttClient.onPublish(onMqttPublish);
   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
   // If your broker requires authentication (username and password), set them below
@@ -150,11 +294,11 @@ void loop() {
     // Publish an MQTT message on topic esp/sparkfuntmp117/celsiustemperature
     uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_CTEMP, 1, true, String(temperatureC).c_str());
     Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", MQTT_PUB_CTEMP, packetIdPub1);
-    Serial.printf("Message: %.2f \n", temperatureC);
+    Serial.printf("Date and Time of Message: %.2f \n", temperatureC);
 
     // Publish an MQTT message on topic esp/sparkfuntmp117/fahrenheittemperature
     uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_FTEMP, 1, true, String(temperatureF).c_str());
-    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", MQTT_PUB_FTEMP, packetIdPub1);
-    Serial.printf("Message: %.2f \n", temperatureF);
+    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", MQTT_PUB_FTEMP, packetIdPub2);
+    Serial.printf("Date and Time of Message: %.2f \n", temperatureF);
   }
 }
